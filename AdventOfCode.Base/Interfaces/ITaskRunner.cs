30a3a6c63dc497f901eac00fd7d94679588be937
Bas Janssen <smh.janssen@gmail.com>
2022-12-04 15:30:48 +0100

    Cleanup bin and obj folders.

--- AdventOfCode.Base/Interfaces/ITaskRunner.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace AdventOfCode.Base.Interfaces;
-
-public interface ITaskRunner
-{
-    Task RunDay(int dayToRun, CancellationToken ctx);
-}
